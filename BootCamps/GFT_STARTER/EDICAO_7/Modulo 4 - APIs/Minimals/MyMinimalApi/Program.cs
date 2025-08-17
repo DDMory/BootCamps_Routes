455555fe9391fdc39da121262a0b3b616c078a3e
DDMory <douglasmory10@gmail.com>
2025-08-17 16:19:46 -0300

    EF table administrador mysql

--- BootCamps/GFT_STARTER/EDICAO_7/Modulo 4 - APIs/Minimals/MyMinimalApi/Program.cs
+++ BootCamps/GFT_STARTER/EDICAO_7/Modulo 4 - APIs/Minimals/MyMinimalApi/Program.cs
@@ -1,7 +1,21 @@
+using Microsoft.EntityFrameworkCore;
 using MyMinimalApi.dominio.DTOs;
+using MyMinimalApi.infraestrutura.db;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+
+builder.Services.AddDbContext<DbContexto>(
+    options =>
+    {
+        options.UseMySql(
+
+          builder.Configuration.GetConnectionString("mysql"),
+          ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("mysql"))
+       );
+    }
+);
+
 
 app.MapGet("/", () => "Hello World!");
 app.MapPost("/login", (LoginDTO loginDTO) =>
--- BootCamps/GFT_STARTER/EDICAO_7/Modulo 4 - APIs/Minimals/MyMinimalApi/infraestrutura/db/DbContexto.cs
+++ BootCamps/GFT_STARTER/EDICAO_7/Modulo 4 - APIs/Minimals/MyMinimalApi/infraestrutura/db/DbContexto.cs
@@ -2,11 +2,35 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MyMinimalApi.dominio.Entidades;
 
 namespace MyMinimalApi.infraestrutura.db
 {
-    public class DbContexto
+    public class DbContexto : DbContext
     {
-        
+        private readonly IConfiguration _configuracaoAppSettings;
+        public DbContexto(IConfiguration configuracaoAppSettings)
+        {
+            _configuracaoAppSettings = configuracaoAppSettings;
+        }
+        public DbSet<Administrador> Administradores { get; set; } = default;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var stringConexao = _configuracaoAppSettings.GetConnectionString("mysql")?.ToString();
+
+            if (!optionsBuilder.IsConfigured){
+                if (!string.IsNullOrEmpty(stringConexao))
+                {
+
+                    optionsBuilder.UseMySql(
+                        stringConexao,
+                        ServerVersion.AutoDetect(stringConexao)
+                    );
+                }
+            
+            }
+        }
     }
 }
